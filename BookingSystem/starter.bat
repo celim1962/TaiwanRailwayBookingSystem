0920575e395ac8b7190c223d2334e5230d391cde
HungyeeLinS3790956 <S3790956@student.rmit.edu.au>
2021-12-13 22:39:45 +0800

    Update starter.bat

--- BookingSystem/starter.bat
+++ BookingSystem/starter.bat
@@ -1,6 +1,8 @@
 echo on
 
-start chrome "127.0.0.1"
+for /F "tokens=2 delims=:" %%i in ('"ipconfig | findstr IP | findstr 192."') do SET LOCAL_IP=%%i
+
+start chrome %LOCAL_IP%
 
 call venv\Scripts\activate.bat
 
@@ -8,7 +10,8 @@ set FLASK_APP=app.py
 
 set FLASK_ENV=development
 
-flask run --port=80
 
+flask run --host=0.0.0.0 --port=80
 
+pause
 
